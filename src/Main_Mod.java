a20a98ca35534e05e9fa6221d58490b24129dce3
Brandon Lee VanderMey <brandon.vandermey@gmail.com>
2015-03-17 02:01:14 -0400

    Complete code for #4

--- src/Main_Mod.java
+++ src/Main_Mod.java
@@ -1,4 +1,33 @@
+//	Main_Mod.java by Kyle Wolff and Brandon VanderMey on 3/17/2015 
 
-public class Main_Mod {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main_Mod
+{
+	
+   public static void main(String[] args)
+   {
+	   Scanner scanner = new Scanner(System.in);
+	   System.out.print("Enter number of elements: ");
+	   int elements = scanner.nextInt();
+	   scanner.nextLine();	//	Gets rid of enter key.
+	   
+	   ArrayList <String> names = new ArrayList<String>(elements);  // <String> is type of object ArrayList can hold
+	   
+	   for (int counter = 0; counter < elements; counter++)
+	   {
+		   System.out.print("Enter name: ");
+		   names.add(scanner.nextLine());
+	   }
+	   
+	   display(names);
+   }
+   
+   public static void display(ArrayList names)
+   {
+      for(int x = 0; x < names.size(); ++x)
+         System.out.println("position " + x + " Name: " +
+            names.get(x));
+   }
 }
